11f9de4bb03b49df196cd48b4c151d33b88589bd
olalindsten@gmail.com <olalindsten@gmail.com>
2016-09-27 16:08:25 +0200

    Skrivit klart menyn

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,6 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+if (foot === col-xs-6){
+    font-size: 10px;
+}
